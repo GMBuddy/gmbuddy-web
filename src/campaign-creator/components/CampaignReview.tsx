bd0f9ca99e1eb1733af6c4ae42533b3deeff0643
Garrick Buckley <gbuckle@purdue.edu>
2016-09-28 01:57:50 -0400

    Fix the guided campaign creator.

--- src/campaign-creator/components/CampaignReview.tsx
+++ src/campaign-creator/components/CampaignReview.tsx
@@ -1,11 +1,41 @@
 import * as React from "react";
 
-class CampaignReview extends React.Component<any, any> {
+import { ICampaignData } from "../components/CampaignDesigner";
+import {connect} from "react-redux";
+import { Table, TableHeader, TableRow, TableHeaderColumn, TableBody, TableRowColumn, Paper } from "material-ui";
+
+interface ICampaignReviewProps {
+    campaignData: ICampaignData;
+}
+
+class CampaignReview extends React.Component<ICampaignReviewProps, any> {
     public render() {
+        const { campaignData } = this.props;
+
         return (
-            <p>Review da campaign.</p>
+            <section>
+                <p>Here's a preview of what will be sent to the server:</p>
+                <Paper>
+                    <Table selectable={false}>
+                        <TableHeader displaySelectAll={false}>
+                            <TableRow>
+                                <TableHeaderColumn>Key</TableHeaderColumn>
+                                <TableHeaderColumn>Value</TableHeaderColumn>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody displayRowCheckbox={false}>
+                            {Object.keys(campaignData).map(key =>
+                                <TableRow key={key} hoverable={true}>
+                                    <TableRowColumn>{key}</TableRowColumn>
+                                    <TableRowColumn>{campaignData[key]}</TableRowColumn>
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </Paper>
+            </section>
         );
     }
 }
 
-export default CampaignReview;
+export default connect()(CampaignReview);
--- src/campaign-creator/containers/CampaignCreator.tsx
+++ src/campaign-creator/containers/CampaignCreator.tsx
@@ -1,62 +1,133 @@
 import * as React from "react";
 
-import { RaisedButton } from "material-ui";
-
 import CampaignStepper from "../components/CampaignStepper";
 
 import CampaignDesigner from "../components/CampaignDesigner";
 import CampaignReview from "../components/CampaignReview";
 import CampaignInvite from "../components/CampaignInvite";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
+
+import { RaisedButton, Paper, Divider } from "material-ui";
+import * as Formsy from "formsy-react";
+
+import { ICampaignData } from "../components/CampaignDesigner";
+import NotFound from "../../layout/components/NotFound";
 
 interface ICampaignCreatorState {
+    campaignData: ICampaignData;
+    canPrevious: boolean;
+    canSubmit: boolean;
     currentStep: number;
 }
 
-class CampaignCreator extends React.Component<any, ICampaignCreatorState> {
-    private steps = {
-        components: [
-            CampaignDesigner,
-            CampaignReview,
-            CampaignInvite,
-        ],
-        names: [
+class CampaignCreator extends React.Component<void, ICampaignCreatorState> {
+    private  steps = [
             "Design Campaign",
             "Review Campaign",
             "Invite Friends",
-        ],
-    };
+        ];
 
     constructor() {
         super();
-        this.state = { currentStep: 0 };
+        this.state = {
+            campaignData: {gameType: null},
+            canPrevious: true,
+            canSubmit: false,
+            currentStep: 0,
+        } as ICampaignCreatorState;
     }
 
     public render() {
-        let nextButton;
+        let buttons = [];
+        let campaignStep;
 
-        if (this.state.currentStep < this.steps.components.length - 1) {
-            nextButton = <RaisedButton onTouchTap={this.nextStep.bind(this)}>Next Step</RaisedButton>;
+        if (this.state.currentStep > 0) {
+            buttons.push(<RaisedButton
+                key="prev"
+                className="campaignCreatorPrevious"
+                onTouchTap={this.previousStep.bind(this)}
+                disabled={!this.state.canPrevious}>Previous</RaisedButton>);
+        }
+
+        if (this.state.currentStep < this.steps.length - 1) {
+            buttons.push(<RaisedButton
+                            key="next"
+                            className="campaignCreatorNext"
+                            type="submit"
+                            disabled={!this.state.canSubmit}>Next</RaisedButton>);
         } else {
-            nextButton = <RaisedButton onTouchTap={this.submit.bind(this)}>Done</RaisedButton>;
+            buttons.push(<RaisedButton
+                            key="done"
+                            className="campaignCreatorSubmit"
+                            type="submit"
+                            primary={true}
+                            disabled={!this.state.canSubmit}>Done</RaisedButton>);
+        }
+
+        switch(this.state.currentStep) {
+            case 0:
+                campaignStep = <CampaignDesigner campaignData={this.state.campaignData}/>;
+                break;
+            case 1:
+                campaignStep = <CampaignReview campaignData={this.state.campaignData}/>;
+                break;
+            case 2:
+                campaignStep = <CampaignInvite/>;
+                break;
+            default:
+                campaignStep = <NotFound/>
+                break;
         }
 
         return (
             <div id="campaign-creator">
                 <h1>Guided Campaign Creator</h1>
-                <CampaignStepper currentStep={this.state.currentStep} names={this.steps.names}/>
-                {React.createElement(this.steps.components[this.state.currentStep])}
-                {nextButton}
+                <Paper>
+                    <CampaignStepper currentStep={this.state.currentStep} names={this.steps}/>
+                    <Divider/>
+                    <Formsy.Form
+                        className="campaignCreatorForm"
+                        onValidSubmit={this.submitForm.bind(this)}
+                        onValid={this.enableSubmit.bind(this)}
+                        onInvalid={this.disableSubmit.bind(this)}
+                    >
+                        <section className="formContent">{campaignStep}</section>
+                        <Divider/>
+                        <section className="buttons">{buttons}</section>
+                    </Formsy.Form>
+                </Paper>
             </div>
         );
     }
 
-    private nextStep() {
-        this.setState({ currentStep: this.state.currentStep + 1 });
+    private enableSubmit() {
+        this.setState({ canSubmit: true } as ICampaignCreatorState);
     }
 
-    private submit() {
-        // console.log("Submit data.");
+    private disableSubmit() {
+        this.setState({ canSubmit: false } as ICampaignCreatorState);
+    }
+
+    private previousStep() {
+        this.setState({ currentStep: this.state.currentStep - 1 } as ICampaignCreatorState);
+    }
+
+    private nextStep() {
+        this.setState({ currentStep: this.state.currentStep + 1 } as ICampaignCreatorState);
+    }
+
+    private submitForm(data) {
+        if (this.state.currentStep < this.steps.length - 1) {
+            if(this.state.currentStep === 0) {
+                this.setState({ campaignData: data } as ICampaignCreatorState);
+            }
+
+            this.nextStep();
+        } else {
+            console.info("Submit data", this.state.campaignData);
+            this.setState({ canPrevious: false } as ICampaignCreatorState);
+            this.disableSubmit();
+        }
     }
 }
 
