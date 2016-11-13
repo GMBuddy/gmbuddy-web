60417c56d022291df1a771c9b336b0eccb87fc16
Travis CI <gbuckle@purdue.edu>
2016-11-12 23:51:42 -0500

    Change API url to reflect if we're in dev or prod.

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,2 +1,12 @@
-export const AUTH_API_URL = "http://localhost:5000";
-export const API_URL = "http://localhost:5001";
+let AUTH_API_URL;
+let API_URL;
+
+if (__DEVTOOLS__) {
+    AUTH_API_URL = "http://localhost:5000";
+    API_URL = "http://localhost:5001";
+} else {
+    AUTH_API_URL = "http://identity.gmbuddy.com";
+    API_URL = "http://api.gmbuddy.com";
+}
+
+export { AUTH_API_URL, API_URL };
