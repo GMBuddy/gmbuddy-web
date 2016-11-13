c264924cc0afa855fc27ec08b552723a841c6929
Travis CI <gbuckle@purdue.edu>
2016-11-13 12:48:28 -0500

    Don't use HTTPS since it's not setup.

--- src/constants.tsx
+++ src/constants.tsx
@@ -5,8 +5,8 @@ if (__DEVTOOLS__) {
     AUTH_API_URL = "http://localhost:5000";
     API_URL = "http://localhost:5001";
 } else {
-    AUTH_API_URL = "https://identity.gmbuddy.com";
-    API_URL = "https://api.gmbuddy.com";
+    AUTH_API_URL = "http://identity.gmbuddy.com";
+    API_URL = "http://api.gmbuddy.com";
 }
 
 export { AUTH_API_URL, API_URL };
