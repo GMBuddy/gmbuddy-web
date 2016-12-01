5697cba09903f6d17a6ef1f7e36f7c8400e7e542
Travis CI <gbuckle@purdue.edu>
2016-12-01 16:13:20 -0500

    Finished up health field.

--- src/character/creator/components/micro20/CharacterDetailsCont.tsx
+++ src/character/creator/components/micro20/CharacterDetailsCont.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import { FormsyText } from "formsy-material-ui/lib";
 import { ICharacterData } from "gmbuddy/micro20/character";
-import { TextField } from "material-ui";
+import { TextField, IconButton } from "material-ui";
+import { PlacesCasino } from "material-ui/svg-icons";
 
 interface ICharacterDetailsContProps extends ICharacterData {
     strength: number;
@@ -10,15 +11,17 @@ interface ICharacterDetailsContProps extends ICharacterData {
 
 interface ICharacterDetailsContState {
     totalHealth: number;
+    strMod: number;
 }
 
 class CharacterDetailsCont extends React.Component<ICharacterDetailsContProps, ICharacterDetailsContState> {
+    constructor(props: any) {
+        super(props);
+        var strMod = Math.floor(((this.props.strength || 0) - 10) / 2);
+        this.state = { totalHealth: strMod, strMod };
+    }
+
     public render() {
-        const strValue = this.props.strength || 0;
-        const strMod = Math.floor((strValue - 10) / 2);
-
-        const health = strMod + this.props.calculated.health;
-
         return (
             <section className="micro20CharacterDetailsCont">
                 <div className="statContainer">
@@ -27,42 +30,57 @@ class CharacterDetailsCont extends React.Component<ICharacterDetailsContProps, I
                         className="statModifier"
                         disabled={true}
                         floatingLabelFixed={true}
-                        value={strMod}
+                        value={this.state.strMod}
                         floatingLabelText="STR Modifier"
                     />
-                    +
+                    <span style={{margin: "10px"}}>+</span>
                     <FormsyText
                         type="number"
-                        key="hp"
+                        key="health"
                         name="calculated.health"
                         value={this.props.calculated.health}
                         validations="isInt"
                         floatingLabelFixed={true}
                         required
-                        floatingLabelText="Health Roll (required)"
-                        onChange={this.updateHealth.bind(this)}
+                        floatingLabelText="Health Roll (1d6 per level) (required)"
+                        onChange={this.updateHealth.bind(this, null)}
                     />
-                    =
+                    <span style={{margin: "10px"}}>=</span>
                     <TextField
                         name="totalhealth"
                         autoComplete="off"
                         floatingLabelText="Character Health"
                         disabled={true}
+                        value={this.state.totalHealth}
                     />
+                    <IconButton
+                        className="rollButton"
+                        onClick={this.roll.bind(this)}
+                        tooltip={"Roll Health"}
+                    ><PlacesCasino/></IconButton>
                 </div>
             </section>
         );
     }
 
-    /* tslint:disable */
-    private componentDidMount() {
-        this.updateHealth();
-    }
-    /* tslint:enable */
+    private roll() {
+        // Roll 1d6 per level.
+        const level = 1; //TODO: replace with actual character's level.
 
-    private updateHealth() {
-        const strMod = Math.floor(((this.props.strength || 0) - 10) / 2);
-        this.setState({totalHealth: this.props.calculated.health + strMod})
+        let rollTotal = 0;
+
+        for (let i = 0; i < level; i++) {
+            rollTotal += Math.ceil(Math.random() * 6);
+        }
+
+        this.props.calculated.health = rollTotal;
+        this.updateHealth(rollTotal, null);
+    }
+
+    private updateHealth(newHealth: number, event) {
+        const health = parseInt(newHealth || event.target.value || 0);
+        this.props.calculated.health = health;
+        this.setState({totalHealth: (health + this.state.strMod)} as ICharacterDetailsContState);
     }
 }
 
